4d3c2c1913ffd20f295936a3fd218500e05f9557
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-07-06 21:48:38 -0700

    update

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -1,10 +1,13 @@
 "use strict";
+const express = require("express");
+const app = express();
+
 module.exports = (app) => {
   app.get("/", (req, res) => {
     res.send("dafsvx");
   });
   app.get("/webhook/", (req, res) => {
-    if (req.query["hub.verify_token"] === "adam") {
+    if (req.query["hub.verify_token"] === "blondiebytes") {
       res.send(req.query["hub.challenge"]);
     }
     res.send("Wrong token");
